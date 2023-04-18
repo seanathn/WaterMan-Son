3758d24071deece9eb242db04a44d73c160de7db
seanathn <seanedbrown@gmail.com>
2023-04-18 12:31:21 -0400

    Testing commit

--- app/src/main/java/com/example/waterman_son/MainActivity.kt
+++ app/src/main/java/com/example/waterman_son/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.waterman_son
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Main Activity", "Testing commit")
     }
 }
