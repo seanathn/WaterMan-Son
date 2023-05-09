02852c9f577b7b3b020331242de7456b25b383b4
seanathn <seanedbrown@gmail.com>
2023-05-09 12:59:30 -0400

    Got creating an account to work correctly. Classwork 5/9/23.

--- app/src/main/java/com/example/waterman_son/LogInSonFragment.kt
+++ app/src/main/java/com/example/waterman_son/LogInSonFragment.kt
@@ -1,17 +1,17 @@
 package com.example.waterman_son
 
 import android.app.appsearch.AppSearchResult.RESULT_OK
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.navigation.findNavController
 import com.example.waterman_son.databinding.FragmentLogInSonBinding
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
@@ -23,6 +23,16 @@ class LogInSonFragment : Fragment() {
     private var _binding : FragmentLogInSonBinding? = null
     private val b get() = _binding!!
     lateinit var dbRef : DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+//            reload()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,58 +42,61 @@ class LogInSonFragment : Fragment() {
 
         dbRef = Firebase.database.reference
 
+        auth = FirebaseAuth.getInstance()
+
         b.movinAndGroovin.setOnClickListener {
-            val action = LogInSonFragmentDirections.actionLogInSonFragmentToWatermanSonMainFragment()
-            b.root.findNavController().navigate(action)
-
-            val signInLauncher = registerForActivityResult(
-                FirebaseAuthUIActivityResultContract(),
-            ) { res ->
-                this.onSignInResult(res)
-            }
-
-            val actionCodeSettings = ActionCodeSettings.newBuilder()
-                .setAndroidPackageName( // yourPackageName=
-                    "com.example.waterman_son", // installIfNotAvailable=
-                    true, // minimumVersion=
-                    null,
-                )
-                .setHandleCodeInApp(true) // This must be set to true
-                .setUrl("https://google.com") // This URL needs to be whitelisted
-                .build()
-
-            val providers = listOf(
-                AuthUI.IdpConfig.EmailBuilder()
-                    .enableEmailLinkSignIn()
-                    .setActionCodeSettings(actionCodeSettings)
-                    .build(),
-            )
-            val signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build()
-            signInLauncher.launch(signInIntent)
-
-//            var googleLogInStuff = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
-//            dbRef.child("username").push().setValue(b.userName.text.toString())
-//            dbRef.child("password").push().setValue(b.userPassword.text.toString())
+            createAccount(b.userName.text.toString(), b.userPassword.text.toString())
         }
 
         return b.root
     }
 
-    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        val response = result.idpResponse
-        if (result.resultCode == RESULT_OK) {
-            // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
-            // ...
-        } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
-        }
+    private fun createAccount(email: String, password: String) {
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+//                    updateUI(user)
+                    val action = LogInSonFragmentDirections.actionLogInSonFragmentToWatermanSonMainFragment()
+                    b.root.findNavController().navigate(action)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        context,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+//                    updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+//                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        context,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+//                    updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
     }
 
     override fun onDestroy() {
