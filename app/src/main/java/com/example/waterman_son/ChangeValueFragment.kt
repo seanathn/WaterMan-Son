bf30a9203d8509004ea14916421c51b8a16d5d95
seanathn <seanedbrown@gmail.com>
2023-05-23 13:01:48 -0400

    Classwork 5/23, app works completely correctly

--- app/src/main/java/com/example/waterman_son/ChangeValueFragment.kt
+++ app/src/main/java/com/example/waterman_son/ChangeValueFragment.kt
@@ -11,6 +11,7 @@ import com.example.waterman_son.databinding.FragmentChangeValueBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.math.RoundingMode
 
 
 class ChangeValueFragment : Fragment() {
@@ -26,11 +27,15 @@ class ChangeValueFragment : Fragment() {
         _binding = FragmentChangeValueBinding.inflate(inflater, container, false)
 
         val args = ChangeValueFragmentArgs.fromBundle(requireArguments())
-        val item = WaterCupSon(args.date, args.amount.toDouble(), args.toString())
+        val amount = args.amount.toDouble().toBigDecimal()
+        val decimal = amount.setScale(2, RoundingMode.HALF_EVEN)
+        val item = WaterCupSon(args.date, decimal.toDouble(), args.time.toString())
         val index = viewModel.waterInfo.value?.indexOf(item)!!
 
+        val userNum = "%.2f".format(args.amount.toDouble())
+
         b.userData.setText(args.date)
-        b.userNumber.setText(args.amount.toDouble().toString())
+        b.userNumber.setText(userNum)
         b.userTime.setText(args.time)
 
         b.removeItem.setOnClickListener {
@@ -41,6 +46,8 @@ class ChangeValueFragment : Fragment() {
 
         b.moveOnSon.setOnClickListener {
             viewModel.replaceItem(index, WaterCupSon(b.userData.text.toString(), b.userNumber.text.toString().toDouble(), b.userTime.text.toString()))
+            dbRef.child("users").child(FirebaseAuth.getInstance().uid.toString()).child("waterList").setValue(viewModel.waterInfo)
+            b.root.findNavController().navigateUp()
         }
 
         return b.root
--- app/src/main/java/com/example/waterman_son/WaterManSonViewModel.kt
+++ app/src/main/java/com/example/waterman_son/WaterManSonViewModel.kt
@@ -10,6 +10,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.math.RoundingMode
 import java.util.stream.IntStream.range
 
 class WaterManSonViewModel: ViewModel() {
